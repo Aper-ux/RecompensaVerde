68d81b3331e2cfbc184fa1f56e4ce2e8da6fd753
Aper-ux <alex.quiroga.perez@gmail.com>
2023-11-11 23:37:01 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,48 @@
+var m1Element = document.getElementById('m1');
+var m2Element = document.getElementById('m2');
+var m3Element = document.getElementById('m3');
+var m4Element = document.getElementById('m4');
+
+m1Element.onclick = function() {
+    document.getElementById('modal1').style.display = 'block';
+};
+
+m2Element.onclick = function() {
+    document.getElementById('modal2').style.display = 'block';
+}
+
+m3Element.onclick = function() {
+    document.getElementById('modal3').style.display = 'block';
+}
+
+m4Element.onclick = function() {
+    document.getElementById('modal4').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal1').style.display = 'none';
+    document.getElementById('modal2').style.display = 'none';
+    document.getElementById('modal3').style.display = 'none';
+    document.getElementById('modal4').style.display = 'none';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const accordionItems = document.querySelectorAll(".accordion-item");
+  
+    accordionItems.forEach(item => {
+      const header = item.querySelector(".accordion-header");
+  
+      header.addEventListener("click", function() {
+        accordionItems.forEach(innerItem => {
+          if (innerItem !== item) {
+            innerItem.classList.remove("active");
+            innerItem.querySelector(".accordion-content").style.display = "none";
+          }
+        });
+  
+        item.classList.toggle("active");
+        const content = item.querySelector(".accordion-content");
+        content.style.display = content.style.display === "block" ? "none" : "block";
+      });
+    });
+  });
