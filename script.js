88fde5bba81e996c9e0f24e1286c8b769b7a1569
Aper-ux <alex.quiroga.perez@gmail.com>
2023-11-12 08:29:57 -0400

    Mapa islas verdes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var m1Element = document.getElementById('m1');
 var m2Element = document.getElementById('m2');
 var m3Element = document.getElementById('m3');
 var m4Element = document.getElementById('m4');
+var m5Element = document.getElementById('m5');
 
 m1Element.onclick = function() {
     document.getElementById('modal1').style.display = 'block';
@@ -19,30 +20,38 @@ m4Element.onclick = function() {
     document.getElementById('modal4').style.display = 'block';
 }
 
+m5Element.onclick = function() {
+    document.getElementById('modal5').style.display = 'block';
+}
+
 function closeModal() {
     document.getElementById('modal1').style.display = 'none';
     document.getElementById('modal2').style.display = 'none';
     document.getElementById('modal3').style.display = 'none';
     document.getElementById('modal4').style.display = 'none';
+    document.getElementById('modal5').style.display = 'none';
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const accordionItems = document.querySelectorAll(".accordion-item");
-  
-    accordionItems.forEach(item => {
+window.onload = function() {
+  const accordionItems = document.querySelectorAll(".accordion-item");
+
+  accordionItems.forEach(item => {
       const header = item.querySelector(".accordion-header");
-  
+      const content = item.querySelector(".accordion-content");
+
       header.addEventListener("click", function() {
-        accordionItems.forEach(innerItem => {
-          if (innerItem !== item) {
-            innerItem.classList.remove("active");
-            innerItem.querySelector(".accordion-content").style.display = "none";
-          }
-        });
-  
-        item.classList.toggle("active");
-        const content = item.querySelector(".accordion-content");
-        content.style.display = content.style.display === "block" ? "none" : "block";
+          accordionItems.forEach(innerItem => {
+              if (innerItem !== item) {
+                  innerItem.classList.remove("active");
+              }
+          });
+
+          item.classList.toggle("active");
       });
-    });
   });
+};
+
+
+
+
+
